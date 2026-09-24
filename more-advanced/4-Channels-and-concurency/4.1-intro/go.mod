d0f27f247d9cae4c6a6f73d56b19c140b260b8b5
Lucalopezz <lucaslops12@gmail.com>
2026-09-24 20:32:26 -0300

    add advanced class 4.1

---
+++ more-advanced/4-Channels-and-concurency/4.1-intro/go.mod
@@ -0,0 +1,3 @@
+module github.com/lucalopezz/GO-classes/more-advanced/4-Channels-concurrency
+
+go 1.27.1
---
+++ more-advanced/4-Channels-and-concurency/4.1-intro/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sendData(ch chan int) {
+	fmt.Println("Sending data...")
+	ch <- 42 // Send data to the channel
+	fmt.Println("Data sent.")
+}
+
+func receiveData(ch chan int) {
+	time.Sleep(500 * time.Millisecond) // Simulate some processing time
+	val := <-ch                        // Receive data from the channel
+	fmt.Println("Received:", val)
+}
+
+func main() {
+	// Comunication between goroutines is done using channels.
+	ch := make(chan int)
+
+	for i := 0; i < 5; i++ {
+		go sendData(ch) // Start a goroutine to send data
+	}
+
+	time.Sleep(1 * time.Second) // Wait for a second to ensure all sends are done
+
+	for i := 0; i < 5; i++ {
+		go receiveData(ch) // Start a goroutine to receive data
+	}
+
+	time.Sleep(1 * time.Second) // Wait for a second to ensure all receives are done
+}
